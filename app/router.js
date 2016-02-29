1726cec144f81e5ae0322bb31ef171a9d06b689a
Ram Chiranjeevi <ramchiranjeevi@gmail.com>
2016-02-29 09:24:20 +0530

    Initial commit of Chat-App
    Initial commit of Chat-App

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('messages', function(){
+    this.route('new');
+  });
 });
 
 export default Router;
